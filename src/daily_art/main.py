5f0142a870688ec555dffef5aa1c535850d2e335
Allan <ningzhong611@gmail.com>
2022-08-31 21:14:35 +0800

    add g

--- src/daily_art/main.py
+++ src/daily_art/main.py
@@ -1,20 +1,33 @@
 import os
+import orjson
+
+from . import g
 
 
 def get_system_info():
     """return the system information"""
-    print("*"*80)
+    print("*" * 80)
     print(f"{os.name}")
     print(f"{os.uname().sysname}")
     print(f"{os.uname().nodename}")
-    print("*"*80)
+    print("*" * 80)
     return os.uname()
 
 
-def add_one(number):
+def get_all_environ() -> bytes:
+    environ_dict = {}
+    for k, v in os.environ.items():
+        environ_dict.update({k: v})
+    print("*" * 80)
+    print(environ_dict)
+    print("*" * 80)
+    return orjson.dumps(environ_dict)
+
+
+def add_one(number: int) -> int:
     return number + 1
-    
+
 
 if __name__ == "__main__":
-    get_system_info()
-    
+    get_all_environ()
+    print(g.url)
